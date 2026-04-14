e418d4e5632c2f60c13d8ea60b71c18ea6e6ebb7
LfAuV <2121271045@qq.com>
2026-04-14 17:46:45 +0800

    Update path params handling

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+"""
+Run Indictate:
+uvicorn main:app --reload
+"""
 app = FastAPI()
+
+
+class ExperimentCreate(BaseModel):
+    name: str
+    status: str
+
 
 @app.get("/")
 def read_root():
@@ -11,5 +21,16 @@ def health_check():
     return {"status": "ok"}
 
 @app.get("/experiment/{experiment_id}")
-def get_experiment(experiment_id: int):
+def get_experiment_list(experiment_id: int):
     return {"experiment_id": experiment_id}
+
+@app.get("/experiment")
+def get_experiment_str(limit: int = 10, status: str = "running"):
+    return {"limit": limit, "status": status}
+
+@app.post("/experiment")
+def create_experiment(experiment: ExperimentCreate):
+    return {
+        "message": "Created",
+        "data": experiment
+    }
